5541cd33234a326097a9826abb132c8d98535668
brian <kimutaibraian922@gmail.com>
2025-02-16 17:12:52 +0300

    ph2

---
+++ src/signup.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    if ($conn->query($sql) {
+        echo "Signup successful!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
